f2f0eb1d2a99834842d5da16aacd67d27ec1f89e
serhii_pmt <sergiy.pmt.20@gmail.com>
2023-10-20 01:06:59 +0300

    add list of items, correct cdn link, script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// get ul with class gallery
+const list = document.querySelector('.gallery');
+
+// create markup
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
