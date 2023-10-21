e2f204e1d9065667c5a1bcdd68037cacdabe64a0
serhii_pmt <sergiy.pmt.20@gmail.com>
2023-10-21 15:24:40 +0300

    finish task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(basicLightbox, galleryItems);
 
 // get ul with class gallery
 const list = document.querySelector('.gallery');
@@ -11,7 +11,7 @@ const markup = galleryItems
   .map(
     ({ preview, original, description }) => `
 <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -23,4 +23,51 @@ const markup = galleryItems
   )
   .join('');
 
+// add all items with links and images to list
 list.insertAdjacentHTML('afterbegin', markup);
+
+// add ivent listener to list
+list.addEventListener('click', handleClick);
+
+// console.dir(list);
+
+// create function for event listener
+function handleClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  // get dataset value from element that was clicked
+  const imageSource = e.target.dataset.source;
+  // get image data from gallery-items that match imageSource
+  const imageData = galleryItems.find(
+    ({ original }) => original === imageSource
+  );
+
+  // create modal with the image that match image that was clicked
+  createModal(imageData);
+}
+
+function createModal({ original, description }) {
+  // create modal with big image
+  const instance = basicLightbox.create(
+    `
+  <img
+      class="gallery__image"
+      src="${original}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  `
+  );
+
+  // show the modal, add event listener that close modal when key Escape clicked
+  instance.show(() => {
+    document.addEventListener('keydown', e => {
+      if (e.key === 'Escape') {
+        instance.close();
+      }
+    });
+  });
+}
